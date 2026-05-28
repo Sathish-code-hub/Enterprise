42bc944c950a08cbcfb1df5897a5603f09e171da
sathish <sathisathish1530@gmail.com>
2026-05-28 20:33:56 +0530

    4th commit

--- src/app/core/services/user.services.ts
+++ src/app/core/services/user.services.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = 'https://vercel.app';
+  private apiUrl = 'https://enterprise-seven-teal.vercel.app';
 
   constructor(private http: HttpClient) {}
 
